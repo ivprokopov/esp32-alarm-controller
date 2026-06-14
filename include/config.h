cc7be253388c845ce0e39f69d4044da085711c05
ivprokopov <iprokopov@gmail.com>
2026-06-14 14:51:36 +0300

    Add alarm configuration header

---
+++ include/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define WIFI_SSID "ALARM_LOCAL"
+#define WIFI_PASSWORD "change_me_123456"
+
+#define ADMIN_CODE "change_me"
+
+#define DISPLAY_ENABLED 0
+#define TFT_CS -1
+#define TFT_DC -1
+#define TFT_RST -1
+#define TFT_BL -1
+
+#define SIREN_PIN 10
+#define ZONE_COUNT 8
